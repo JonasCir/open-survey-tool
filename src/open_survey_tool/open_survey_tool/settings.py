85010eaae5c0477d1fd61174cccdbb2ebf09bdfc
Nils Vossebein <nv@vipfy.com>
2021-03-21 10:52:23 +0100

    fix db back to sqlite 3

--- src/open_survey_tool/open_survey_tool/settings.py
+++ src/open_survey_tool/open_survey_tool/settings.py
@@ -78,12 +78,9 @@ WSGI_APPLICATION = 'open_survey_tool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'NAME': os.getenv('DB_NAME', 'open_survey'),
-        'USER': os.getenv('DB_USER', 'open_survey_user'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'password')
-    },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
--- src/open_survey_tool/results/figures/rating_distribution.py
+++ src/open_survey_tool/results/figures/rating_distribution.py
@@ -8,43 +8,43 @@ from results.models import SurveyResult
 class RatingDistribution(Figure):
 
     @staticmethod
-    def get_html(cfg, mode=None)):
-        res=RatingDistribution.compute(mode)
-        fig=px.bar(res, labels = {'value': 'Anzahl'})
+    def get_html(cfg, mode=None):
+        res = RatingDistribution.compute(mode)
+        fig = px.bar(res, labels={'value': 'Anzahl'})
 
-        fig.update_xaxes(type = 'category')
-        fig.update_yaxes(tickformat = ',d', automargin = False)
+        fig.update_xaxes(type='category')
+        fig.update_yaxes(tickformat=',d', automargin=False)
 
         return fig.to_html(**cfg)
 
     @ staticmethod
-    def compute(mode = None):
+    def compute(mode=None):
         # get all ratings from the DB
-        df=pd.DataFrame.from_records(
+        df = pd.DataFrame.from_records(
             map(lambda x: x['result'], SurveyResult.objects.all().values()))
 
         # group ratings by counts
         res = df[mode or 'satisfaction'].value_counts().rename(
             'Personen').to_frame()
-        res.index.rename('Bewertung', inplace = True)
+        res.index.rename('Bewertung', inplace=True)
 
         # fill missing ratings
         if mode == "question1-1":
-            res=res.reindex(["item1", "item2", "item3",
-                               "item4"], fill_value = 0)
+            res = res.reindex(["item1", "item2", "item3",
+                               "item4"], fill_value=0)
         else:
-            res=res.reindex(["item1", "item2", "item3",
-                               "item4", "item5"], fill_value = 0)
+            res = res.reindex(["item1", "item2", "item3",
+                               "item4", "item5"], fill_value=0)
 
         if mode == "question1-1":
-            res=res.rename({"item1": "Anzeigenerstatter:in", "item2": "Beschuldigte(r)", "item3": "Zeug(e):in",
-                                    "item4": "Geschädigte(r)"}, axis = 'index')
+            res = res.rename({"item1": "Anzeigenerstatter:in", "item2": "Beschuldigte(r)", "item3": "Zeug(e):in",
+                              "item4": "Geschädigte(r)"}, axis='index')
 
         elif mode == "question1-2":
-            res=res.rename({"item1": "Straßenverkehr allgemein", "item2": "Internetkriminalität", "item3": "Körperverletzungsdelikt",
-                                    "item4": "Eigentumsdelikt", "item5": "Delikt gegen die sexuelle Selbstbestimmung"}, axis = 'index')
+            res = res.rename({"item1": "Straßenverkehr allgemein", "item2": "Internetkriminalität", "item3": "Körperverletzungsdelikt",
+                              "item4": "Eigentumsdelikt", "item5": "Delikt gegen die sexuelle Selbstbestimmung"}, axis='index')
         else:
-            res=res.rename({"item1": "Trifft voll zu", "item2": "Trifft zu", "item3": "Trifft weniger zu",
-                                    "item4": "Trifft gar nicht zu", "item5": "Keine Angabe"}, axis = 'index')
+            res = res.rename({"item1": "Trifft voll zu", "item2": "Trifft zu", "item3": "Trifft weniger zu",
+                              "item4": "Trifft gar nicht zu", "item5": "Keine Angabe"}, axis='index')
 
         return res
