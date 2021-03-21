aaf6414f2e5a512b9f8a75f19298515ea6e7cf70
Jonas Cirotzki <jonas.cirotzki@posteo.de>
2021-03-21 16:33:10 +0100

    enable postgres

--- src/open_survey_tool/open_survey_tool/settings.py
+++ src/open_survey_tool/open_survey_tool/settings.py
@@ -78,9 +78,12 @@ WSGI_APPLICATION = 'open_survey_tool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'NAME': os.getenv('DB_NAME', 'open_survey'),
+        'USER': os.getenv('DB_USER', 'open_survey_user'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'password')
+    },
 }
 
 # Password validation
@@ -150,4 +153,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "staticfiles"),
 ]
-
